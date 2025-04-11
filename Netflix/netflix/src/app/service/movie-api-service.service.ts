4481918fa200d2b7699e73e067d58dc72ad7b42c
shubhamkaul1999 <rukendo2@gmail.com>
2025-04-11 19:02:24 +0530

    Initial commit

---
+++ Netflix/netflix/src/app/service/movie-api-service.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieApiServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  baseurl = "https://api.themoviedb.org/3";
+  apikey = "08cc33bd5ae3a747598ce2ad84376e66";
+
+  // bannerapidata
+  bannerApiData(): Observable<any> {
+    return this.http.get(`${this.baseurl}/trending/all/week?api_key=${this.apikey}`);
+  }
+
+  // trendingmovieapidata 
+  trendingMovieApiData(): Observable<any> {
+    return this.http.get(`${this.baseurl}/trending/movie/day?api_key=${this.apikey}`);
+  }
+
+  // searchMovie
+  getSearchMovie(data:any): Observable<any> {
+    return this.http.get(`${this.baseurl}/search/movie?api_key=${this.apikey}&query=${data.movieName}`);
+  }
+
+  // movieDetails
+  getMovieDetails(data:any): Observable<any> {
+    return this.http.get(`${this.baseurl}/movie/${data}?api_key=${this.apikey}`);
+  }
+
+  // movieVideo
+  getMovieVideo(data:any): Observable<any> {
+    return this.http.get(`${this.baseurl}/movie/${data}/videos?api_key=${this.apikey}`);
+  }
+
+  // movieCast
+  getMovieCast(data:any): Observable<any> {
+    return this.http.get(`${this.baseurl}/movie/${data}/credits?api_key=${this.apikey}`);
+  }
+
+}
